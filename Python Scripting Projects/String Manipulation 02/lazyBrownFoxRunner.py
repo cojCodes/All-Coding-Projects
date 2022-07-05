d65c9198a2ac2ca0e727a3c650268d4b3afc3151
Christofer Jones <97864746+cojCodes@users.noreply.github.com>
2022-07-05 16:02:56 -0500

    Create lazyBrownFoxRunner.py

---
+++ Python Scripting Projects/String Manipulation 02/lazyBrownFoxRunner.py
@@ -0,0 +1,14 @@
+# Christofer Jones
+# Proj06Runner
+
+def run(str, sep):
+    print('Christofer Jones')
+    
+    print(str)
+    print(sep)
+    
+    separation_point = str.index(sep)
+
+    split_string = str.split(sep)
+        
+    return split_string
