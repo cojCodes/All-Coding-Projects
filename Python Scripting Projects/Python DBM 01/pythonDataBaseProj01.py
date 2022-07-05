f2fb1c7dfee462f4bd65844ebf62d617bf62a389
Christofer Jones <97864746+cojCodes@users.noreply.github.com>
2022-07-05 18:35:16 -0500

    Delete Python Scripting Projects/Python DBM 01 directory

--- Python Scripting Projects/Python DBM 01/pythonDataBaseProj01.py
+++
@@ -1,9 +0,0 @@
-import Proj10Runner
-
-myKeys = b'name',b'age',b'gender'
-myValues = b'Joe',b'39',b'male'
-databaseName = 'database'
-
-Proj10Runner.populateIt(myKeys,myValues,databaseName)
-
-Proj10Runner.printIt(myKeys,myValues,databaseName)
